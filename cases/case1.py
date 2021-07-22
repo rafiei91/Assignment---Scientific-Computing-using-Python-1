5bcd6dba7a3d3c833cf131b6e924d1ed77d887b6
rafiei91 <77726877+rafiei91@users.noreply.github.com>
2021-07-22 13:17:37 +0200

    Update case1.py

--- cases/case1.py
+++ cases/case1.py
@@ -4,7 +4,6 @@ This file contains the necessary calls to make plots etc for case 1
 
 import sys
 import os
-import lorenz
 
 delta = 10
 beta = 8 * (1 / 3)
@@ -17,5 +16,6 @@ U_0 = [-1,-1,20]    # Initial Conditions
 f = Lorenz_a   # ODE
 
 sys.path.append('../')
+import lorenz
 
 lorenz.run.simulate(f,U_0,'case1',False)
