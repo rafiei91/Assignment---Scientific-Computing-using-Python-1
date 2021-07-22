75c7fe7981343b7a9cbf61d8fe156bdeaa759600
rafiei91 <77726877+rafiei91@users.noreply.github.com>
2021-07-22 13:04:05 +0200

    Update case2.py

--- cases/case2.py
+++ cases/case2.py
@@ -3,3 +3,32 @@ This file could contain the necessary calls to make plots etc for
 case 2
 
 """
+import sys
+import os
+import lorenz
+
+sys.path.append('../')
+
+def ode_lorenz_attractor1(X, t):
+    # Define parameter
+    x, y, z = X
+
+    # Case 2
+    delta = 10
+    beta = 8 * (1 / 3)
+    rho = 16
+
+    dxdt = delta * (y - x)
+    dydt = x * (rho - z) - y
+    dzdt = x * y - beta * z
+
+    # the function returns the vector [dxdt, dydt, dzdt]
+    return [dxdt, dydt, dzdt]
+
+if os.path.exists('case2') is False:
+    os.mkdir('case2')
+
+U_0 = [-1,-1,20]    # Initial Conditions
+f = ode_lorenz_attractor1   # ODE
+
+lorenz.run.simulate(f,U_0,'case2',False)
