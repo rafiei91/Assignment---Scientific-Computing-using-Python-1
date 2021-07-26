c569da5e34a4186b518ac3308940201dabf4284b
rafiei91 <77726877+rafiei91@users.noreply.github.com>
2021-07-26 14:49:04 +0200

    Update solver.py

--- lorenz/solver.py
+++ lorenz/solver.py
@@ -3,11 +3,9 @@ This file contains the ODE solver
 """
 import numpy as np
 
+def ode_solver(f, c0, dt=0.01, N_t=50000): # ODE solver by simple Newton Euler algorithm
 
-def ode_solver(f, c0, dt=0.01, T=50000 * 0.01): # ODE solver by simple Newton Euler algorithm
-
-    N_t = int(round(float(T) / dt))
-    # Ensure that any list/tuple returned from f_ is wrapped as array
+    T = N_t * dt
     f_ = lambda u, t: np.asarray(f(u, t))
     u = np.zeros((N_t + 1, len(c0)))
     t = np.linspace(0, N_t * dt, len(u))
