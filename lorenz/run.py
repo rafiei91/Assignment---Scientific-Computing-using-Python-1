d00a1c86b9653c450d7e24d49993f5b872487356
rafiei91 <77726877+rafiei91@users.noreply.github.com>
2021-07-26 10:54:04 +0200

    Update run.py

--- lorenz/run.py
+++ lorenz/run.py
@@ -1,36 +1,19 @@
 """
 This file contains the interface/function for
-
-1: computing a trajectory using an ODE solver from solver.py
-2: save data to file
-3: plot data
-
 """
+
 from . import filehandling as fh
 from . import plot
 from . import solver
 
+def simulate(f, c0, case, s): # Execute ODE solver and call the saver and ploters
 
-def simulate(f, U_0, name, s):
-    """
-    Interfacing function for executing different scenarios.
-
-    INPUT::
-
-     f: ODE function in the form of f(X,t). X can be an array of the form [a, b, c ...]
-     U_0: initial condition. U_0 = [a0, b0, c0, ...]
-     name: name of the file
-
-    OUPUT::
-
-    """
-
-    u, t = solver.ode_solver(f, U_0)
+    u, t = solver.ode_solver(f, c0)
 
     x = u[:, 0]
     y = u[:, 1]
     z = u[:, 2]
 
-    fh.save_data([x, y, z, t], name)
-    plot.plot2d(x, y, z, name, s)
-    plot.plot3d(x, y, z, name, s)
+    fh.save_data([x, y, z, t], case)
+    plot.plot2d(x, y, z, case, s)
+    plot.plot3d(x, y, z, case, s)
