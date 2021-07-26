204c582a58693accf3e6dcf0e3f4f53e5552ffc6
rafiei91 <77726877+rafiei91@users.noreply.github.com>
2021-07-26 10:46:35 +0200

    Update plot.py

--- lorenz/plot.py
+++ lorenz/plot.py
@@ -4,42 +4,32 @@ This file contains functionalities for plotting
 
 import matplotlib.pyplot as plt
 
-def plot3d(a, b, c, name, s): # make a pdf file for the 3D plot
+def plot3d(a, b, c, case, s): # make a pdf file for the 3D plot
 
-    # Initialize a figure
     fig = plt.figure()
-
-    ax = fig.add_subplot(projection='3d')
-    ax.plot(a, b, c, label='Lorenz attractor')
-
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
-
-    if s is True:
-        plt.show()
-
-    plt.savefig(name + "/3D.pdf")
-
-def plot2d(a, b, c, name, s): # make a pdf file for three 2D plots
     
-    # Initialize a figure
+    ax = fig.add_subplot(projection='3d')
+    ax.plot(a, b, c, color='r')
+    fig.suptitle('3D Lorenz Attractor plot for ' + case)
+
+    ax.set(xlabel='x', ylabel='z', zlabel='z')
+
+    plt.savefig(case + "/" + case + " - 3D.pdf")
+
+def plot2d(a, b, c, case, s): # make a pdf file for three 2D plots
+    
     fig, axs = plt.subplots(3, figsize=(8, 10))
-    fig.suptitle('Lorenz Attractor ' + name)
+    fig.suptitle('2D Lorenz Attractor plot for ' + case)
 
-    axs[0].plot(a, b)
-    axs[0].set_xlabel('X')
-    axs[0].set_ylabel('Y')
+    axs[0].plot(a, b, color='r')
+    axs[0].set(xlabel='x', ylabel='y', title='(x-y) plot')
 
-    axs[1].plot(a, c)
-    axs[1].set_xlabel('X')
-    axs[1].set_ylabel('Z')
+    axs[1].plot(a, c, color='r')
+    axs[1].set(xlabel='x', ylabel='z', title='(x-z) plot')
 
-    axs[2].plot(c, b)
-    axs[2].set_xlabel('Y')
-    axs[2].set_ylabel('Z')
+    axs[2].plot(c, b, color='r')
+    axs[2].set(xlabel='y', ylabel='z', title='(y-z) plot')
 
-    if s is True:
-        plt.show()
+    fig.subplots_adjust(hspace=0.5)
 
-    plt.savefig(name + "/2D.pdf")
+    plt.savefig(case + "/" + case + " - 2D.pdf")
