8d4ec54fc354d6e741e4e969713ff2bbf24bf2c3
rafiei91 <77726877+rafiei91@users.noreply.github.com>
2021-07-27 08:51:09 +0200

    Update filehandling.py

--- lorenz/filehandling.py
+++ lorenz/filehandling.py
@@ -3,7 +3,7 @@ This file can contain functionalities for saving/loading data
 """
 import h5py
 
-def save_data(file, case): # hdf5 file saving
+def saveing(file, case): # hdf5 file saving
 
     f = h5py.File(case + "/" + case + '.hdf5', 'w')
     f.create_dataset('RND', data=file)
