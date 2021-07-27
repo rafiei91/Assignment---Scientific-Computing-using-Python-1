377f67d86dba032e10c1c59d68bdbf7aae34e25b
rafiei91 <77726877+rafiei91@users.noreply.github.com>
2021-07-27 09:22:28 +0200

    Update plot.py

--- lorenz/plot.py
+++ lorenz/plot.py
@@ -9,7 +9,7 @@ def plot_3D(a, b, c, case, s): # make a pdf file for the 3D plot
     fig = plt.figure()
     
     ax = fig.add_subplot(projection='3d')
-    ax.plot(a, b, c, color='r')
+    ax.plot(a, b, c, color='r', linewidth=0.05)
     fig.suptitle('3D Lorenz Attractor plot for ' + case)
 
     ax.set(xlabel='x', ylabel='y', zlabel='z')
@@ -21,13 +21,13 @@ def plot_2D(a, b, c, case, s): # make a pdf file for three 2D plots
     fig, axs = plt.subplots(3, figsize=(8, 10))
     fig.suptitle('2D Lorenz Attractor plot for ' + case)
 
-    axs[0].plot(a, b, color='r')
+    axs[0].plot(a, b, color='r', linewidth=0.05)
     axs[0].set(xlabel='x', ylabel='y', title='(x-y) plot')
 
-    axs[1].plot(a, c, color='r')
+    axs[1].plot(a, c, color='r', linewidth=0.05)
     axs[1].set(xlabel='x', ylabel='z', title='(x-z) plot')
 
-    axs[2].plot(b, c, color='r')
+    axs[2].plot(b, c, color='r', linewidth=0.05)
     axs[2].set(xlabel='y', ylabel='z', title='(y-z) plot')
 
     fig.subplots_adjust(hspace=0.5)
