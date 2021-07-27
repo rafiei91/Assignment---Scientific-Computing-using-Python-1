0f457e66702979421a4f3104897e2174300516ad
rafiei91 <77726877+rafiei91@users.noreply.github.com>
2021-07-27 08:48:56 +0200

    Update case3.py

--- case/case3.py
+++ case/case3.py
@@ -27,4 +27,4 @@ if os.path.exists('case3') is False:
 c0 = [-1,-1,20]    # Initial Conditions
 f = ode_lorenz_attractor   # ODE
 
-lorenz.run.simulate(f,c0,'case3',False)
+lorenz.run.execute(f,c0,'case3',False)
